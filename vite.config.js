32a6098d42f196a52d6939427d06e48676c1de28
Hernan Gonzalez <hernangozalez@gmail.com>
2025-11-17 14:31:16 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [react()],
   
-  // Para GitHub Pages en subdirectorio con HashRouter
+  // Cambiar de './' a '/ZunyFernandezPropiedades/'
   base: '/ZunyFernandezPropiedades/',
 
   resolve: {
@@ -13,15 +13,6 @@ export default defineConfig({
       '/components': fileURLToPath(new URL('./src/components', import.meta.url)),
       '/pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
       '/utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
-    }
-  },
-  
-  // IMPORTANTE: Asegurar que los assets públicos usen rutas correctas
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
     }
   }
 })
