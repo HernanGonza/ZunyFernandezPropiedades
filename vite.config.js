8c077cb34cc122b3e5c7f73ee37e8781d3c4df8c
Hernan Gonzalez <hernangozalez@gmail.com>
2025-12-02 07:07:09 -0300

    ruta

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [react()],
   
-  // Para dominio propio:
-  base: '/',
+  // Cambiar de './' a '/ZunyFernandezPropiedades/'
+  base: '/ZunyFernandezPropiedades/',
 
   resolve: {
     alias: {
